11f75cd308d18a275cc584c0ba4d92e0fe0aa20d
Mark Wetzel <mark@markwetzel.com>
2013-03-17 13:11:06 -0700

    Initial commit.

---
+++ Piggy/Piggy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Piggy
+{
+    static class Piggy
+    {
+        public static string Convert(string input)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            // Strip any punctuation. 
+            input = Regex.Replace(input, @"[^\w\s]", "");
+
+            foreach (string word in input.Split(' ')) {
+                char firstLetter = word[0];
+                string pigFormat = firstLetter.IsVowel() ?
+                    string.Format("{0}-ay", word) :
+                    string.Format("{0}-{1}ay", word.Substring(1, word.Length - 1), firstLetter);
+                builder.Append(pigFormat + " ");
+            }
+            return builder.ToString();
+        }
+    }
+}
---
+++ Piggy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace Piggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Piggy!\n\n(Ctrl+C to quit)\n");
+
+            string input = string.Empty;
+
+            while (true) {
+                do {
+                    Console.Write("Enter some text to convert to pig latin: ");
+                    // Get the input and get rid of any excess whitespace.
+                    input = Console.ReadLine().Trim();
+                } while (string.IsNullOrWhiteSpace(input));
+
+                Console.WriteLine(Piggy.Convert(input));
+            }
+        }
+    }
+}
